8843d6a4f8ccf8ab8ebf7f35c16d8c7393fdb28d
oldgear98 <oldgear98@gmail.com>
2021-03-29 15:47:13 +0900

    TEST

---
+++ pack.py
@@ -0,0 +1,14 @@
+print("pack man game strat")
+print("유령이 나타 났다")
+print("1.run away 2.use item 3.kill")
+number = int(input("숫자를 입력하세요:"))
+#만약에 유저가 입력한 글자가 1 도망.
+#만약에 유저가 입력한 글자가 2 아이템.
+#만약에 유져가 입력한 글자가 3 싸운다.
+if number == 1:
+    print("도망갔지만 잡혀버렸다.")
+elif number == 2:
+        print("아이템을 사용했다.")
+elif number == 3:
+            print("싸웠지만 패배했다.")
+            
